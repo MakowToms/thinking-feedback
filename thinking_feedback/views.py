0f8e19f8ee835abf2629125c20c195fd1ad8de43
MakowToms <tomasz.tu.tm@gmail.com>
2023-09-12 00:45:05 +0200

    use auto-analyzers

--- thinking_feedback/views.py
+++ thinking_feedback/views.py
@@ -1,23 +1,11 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.admin.views.decorators import staff_member_required
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.template.loader import get_template
-from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.contrib import messages
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth import authenticate, get_user_model, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.template.loader import get_template
 
 from topic.models import Topic
 
 qs = Topic.objects.all()
-
-
-
-        
-
-
-
-
-
-
-
-
